5081ac1bf975d1536554eafa9310505ca16a9993
climw <wu08518@126.com>
2018-01-16 23:57:50 +0800

    deepstart

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <div>欢迎来到医指康121222222222222</div>
+    <div>
+      <el-button round>圆形按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </div>
+    <div>欢迎来到医指康</div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  components: {
+    // AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/shopingcar/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>我是购物车</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
